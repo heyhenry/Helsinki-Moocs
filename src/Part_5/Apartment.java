eaa8a8ee016848c67a6a09257c95f830c36974ea
Nampa <henryyy3@hotmail.com>
2020-07-30 01:20:32 +0800

    Completed an exercise from part five.

--- src/Part_5/Apartment.java
+++ src/Part_5/Apartment.java
@@ -38,28 +38,42 @@ public class Apartment {
 
     public int priceDifference(Apartment compared) {
 
-        return Math.abs(this.pricePerSquare - compared.pricePerSquare);
+        return Math.abs(this.pricePerSquare * this.squares - compared.pricePerSquare * compared.squares);
+
+    }
+
+    public boolean moreExpensiveThan(Apartment compared) {
+
+        if(this.pricePerSquare * this.squares > compared.pricePerSquare * compared.squares) {
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
 
     }
 
     public static void main(String[] args) {
 
-        //part 1
-//        Apartment manhattanStudioApt = new Apartment(1, 16, 5500);
-//        Apartment atlantaTwoBedroomApt = new Apartment(2, 38, 4200);
-//        Apartment bangorThreeBedroomApt = new Apartment(3, 78, 2500);
-//
-//        System.out.println(manhattanStudioApt.largerThan(atlantaTwoBedroomApt));     // false
-//        System.out.println(bangorThreeBedroomApt.largerThan(atlantaTwoBedroomApt));  // true
-//
-//        Apartment manhattanStudioApt = new Apartment(1, 16, 5500);
-//        Apartment atlantaTwoBedroomApt = new Apartment(2, 38, 4200);
-//        Apartment bangorThreeBedroomApt = new Apartment(3, 78, 2500);
-//
-//        System.out.println(manhattanSingleRoomApt.priceDifference(atlantaTwoRoomApt));  //71600
-//        System.out.println(bangorThreeRoomApt.priceDifference(atlantaTwoRoomApt));   //35400
+        Apartment manhattanStudioApt = new Apartment(1, 16, 5500);
+        Apartment atlantaTwoBedroomApt = new Apartment(2, 38, 4200);
+        Apartment bangorThreeBedroomApt = new Apartment(3, 78, 2500);
+
+        // tests method largerThan()
+        System.out.println(manhattanStudioApt.largerThan(atlantaTwoBedroomApt));     // false
+        System.out.println(bangorThreeBedroomApt.largerThan(atlantaTwoBedroomApt));  // true
+
+        // tests method priceDifference()
+        System.out.println(manhattanStudioApt.priceDifference(atlantaTwoBedroomApt));  //71600
+        System.out.println(bangorThreeBedroomApt.priceDifference(atlantaTwoBedroomApt));   //35400
+
+        // tests method moreExpensiveThan()
+        System.out.println(manhattanStudioApt.moreExpensiveThan(atlantaTwoBedroomApt));  // false
+        System.out.println(bangorThreeBedroomApt.moreExpensiveThan(atlantaTwoBedroomApt));   // true
 
     }
-
 
 }
